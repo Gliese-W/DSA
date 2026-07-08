fa66e809fdec15a2bc8fe9c8a708923bd3f02f4a
Gliese-W <always.tusharr@gmail.com>
2026-07-08 11:24:46 +0530

    Time: 130 ms (5.08%), Space: 28.5 MB (63.61%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -4,32 +4,59 @@ public:
 
         vector<int> nge;
 
+        int N = nums.size();
         for (int i=0; i<nums.size(); i++){
-
             bool flag = 0;
-
-            for (int j=i+1; j<nums.size(); j++){
-                if (nums[j] > nums[i]){
-                    nge.push_back(nums[j]);
+            for (int j=i+1; j <= i+N-1; j++){
+                int ind = j % N;
+                if (nums[ind] > nums[i]){
+                    nge.push_back(nums[ind]);
                     flag = 1;
                     break;
                 }
             }
-            
-            if (!flag) {
-                for (int k=0; k<i; k++){
-                    if (nums[k] > nums[i]){
-                        nge.push_back(nums[k]);
-                        flag = 1;
-                        break;
-                    }
-                }
-            }
 
-            if (!flag){
+            if (flag == 0){
                 nge.push_back(-1);
             }
         }
+
         return nge;
+
+
+
+
+
+
+
+        // vector<int> nge;
+
+        // for (int i=0; i<nums.size(); i++){
+
+        //     bool flag = 0;
+
+        //     for (int j=i+1; j<nums.size(); j++){
+        //         if (nums[j] > nums[i]){
+        //             nge.push_back(nums[j]);
+        //             flag = 1;
+        //             break;
+        //         }
+        //     }
+            
+        //     if (!flag) {
+        //         for (int k=0; k<i; k++){
+        //             if (nums[k] > nums[i]){
+        //                 nge.push_back(nums[k]);
+        //                 flag = 1;
+        //                 break;
+        //             }
+        //         }
+        //     }
+
+        //     if (!flag){
+        //         nge.push_back(-1);
+        //     }
+        // }
+        // return nge;
     }
 };

8e1ad339ebee2c84dd06169206418746bfdadbb1
Gliese-W <always.tusharr@gmail.com>
2026-07-08 11:06:45 +0530

    Time: 5 ms (13.88%), Space: 12.8 MB (62.72%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -2,37 +2,71 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
 
-        vector<int> st;
+        stack<int> st;
+        vector<int> nge;
 
-        int i=0, j=0;
-        
+        unordered_map<int, int> mp;
 
-        while (i<nums1.size()){
-            bool flag = 0;
-            while(j<nums2.size()){
-                if (nums1[i] != nums2[j]){
-                    j++;
-                }else{
-                    for (int k=j+1; k<nums2.size(); k++){
-                        if (nums2[k]>nums2[j]){
-                            st.push_back(nums2[k]);
-                            flag=1;
-                            break;
-                        } 
-                    }
 
-                    if(!flag){
-                        st.push_back(-1);
-                        flag = 1;
-                    }
-                }
+        for (int j=nums2.size()-1; j>=0; j--){
 
-                if(flag==1) break;
+            while (!st.empty() && st.top() <= nums2[j]){
+                st.pop();
+            } 
+
+            if (st.empty()){
+                mp[nums2[j]] = -1;
+            } else{
+                mp[nums2[j]] = st.top();
             }
-            j=0;
-            i++;
+
+            st.push(nums2[j]);
         }
 
-        return st;
+        for (auto ele : nums1){
+            nge.push_back(mp[ele]);
+        }
+        
+
+        return nge;
+
+
+
+
+
+
+        // vector<int> st;
+
+        // int i=0, j=0;
+        
+
+        // while (i<nums1.size()){
+        //     bool flag = 0;
+        //     while(j<nums2.size()){
+        //         if (nums1[i] != nums2[j]){
+        //             j++;
+        //         }else{
+        //             for (int k=j+1; k<nums2.size(); k++){
+        //                 if (nums2[k]>nums2[j]){
+        //                     st.push_back(nums2[k]);
+        //                     flag=1;
+        //                     break;
+        //                 } 
+        //             }
+
+        //             if(!flag){
+        //                 st.push_back(-1);
+        //                 flag = 1;
+        //             }
+        //         }
+
+        //         if(flag==1) break;
+        //     }
+        //     j=0;
+        //     i++;
+        // }
+
+        // return st;
+
     }
 };

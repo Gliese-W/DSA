506b85e23549e7365167b547291921b1702f66f5
Gliese-W <always.tusharr@gmail.com>
2026-07-12 11:29:54 +0530

    Time: 1 ms (53.86%), Space: 13 MB (15.65%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,72 +1,29 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-
         stack<int> st;
         vector<int> nge;
 
         unordered_map<int, int> mp;
 
-
-        for (int j=nums2.size()-1; j>=0; j--){
-
-            while (!st.empty() && st.top() <= nums2[j]){
+        for(int i=nums2.size()-1; i>=0; i--){
+            while (!st.empty() && st.top() <= nums2[i]){
                 st.pop();
             } 
 
             if (st.empty()){
-                mp[nums2[j]] = -1;
-            } else{
-                mp[nums2[j]] = st.top();
+                mp[nums2[i]] = -1;
+            }else{
+                mp[nums2[i]] = st.top();
             }
 
-            st.push(nums2[j]);
+            st.push(nums2[i]);
         }
 
-        for (auto ele : nums1){
-            nge.push_back(mp[ele]);
+        for (int i : nums1){
+            nge.push_back(mp[i]);
         }
-        
 
         return nge;
-
-
-
-
-
-
-        // vector<int> st;
-
-        // int i=0, j=0;
-        
-
-        // while (i<nums1.size()){
-        //     bool flag = 0;
-        //     while(j<nums2.size()){
-        //         if (nums1[i] != nums2[j]){
-        //             j++;
-        //         }else{
-        //             for (int k=j+1; k<nums2.size(); k++){
-        //                 if (nums2[k]>nums2[j]){
-        //                     st.push_back(nums2[k]);
-        //                     flag=1;
-        //                     break;
-        //                 } 
-        //             }
-
-        //             if(!flag){
-        //                 st.push_back(-1);
-        //                 flag = 1;
-        //             }
-        //         }
-
-        //         if(flag==1) break;
-        //     }
-        //     j=0;
-        //     i++;
-        // }
-
-        // return st;
-
     }
 };

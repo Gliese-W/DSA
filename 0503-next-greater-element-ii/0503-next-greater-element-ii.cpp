51ad4310074bd5d2da72473b3419ddc875e78acc
Gliese-W <always.tusharr@gmail.com>
2026-07-12 11:35:31 +0530

    Time: 83 ms (7.39%), Space: 28.4 MB (83.87%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -2,89 +2,33 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
 
+        vector<int> nge;
 
-        vector<int> nge(nums.size());
-        stack<int> st;
-
-        for (int i=2*nums.size()-1; i>=0; i--){
-
-            while (!st.empty() && st.top() <= nums[i%nums.size()]){
-                st.pop();
+        for (int i=0; i<nums.size(); i++){
+            bool flag = 0;
+            for (int j=i+1; j<nums.size(); j++){
+                if (nums[j] > nums[i]){
+                    nge.push_back(nums[j]);
+                    flag = 1;
+                    break;
+                }
             }
 
-            if (i<nums.size()){
-
-                nge[i] = st.empty() ? -1 : st.top();
-
-                // if (st.empty()){
-                //     nge.push_back(-1);
-                // } else{
-                //     nge.push_back(st.top());
-                // }
+            if (!flag){
+                for (int j=0; j<i; j++){
+                    if (nums[j] > nums[i]){
+                        nge.push_back(nums[j]);
+                        flag = 1;
+                        break;
+                    }
+                }
             }
-            st.push(nums[i%nums.size()]);
+
+            if (!flag){
+                nge.push_back(-1);
+            }
         }
 
         return nge;
-
-
-
-
-        // vector<int> nge;
-
-        // int N = nums.size();
-        // for (int i=0; i<nums.size(); i++){
-        //     bool flag = 0;
-        //     for (int j=i+1; j <= i+N-1; j++){
-        //         int ind = j % N;
-        //         if (nums[ind] > nums[i]){
-        //             nge.push_back(nums[ind]);
-        //             flag = 1;
-        //             break;
-        //         }
-        //     }
-
-        //     if (flag == 0){
-        //         nge.push_back(-1);
-        //     }
-        // }
-
-        // return nge;
-
-
-
-
-
-
-
-        // vector<int> nge;
-
-        // for (int i=0; i<nums.size(); i++){
-
-        //     bool flag = 0;
-
-        //     for (int j=i+1; j<nums.size(); j++){
-        //         if (nums[j] > nums[i]){
-        //             nge.push_back(nums[j]);
-        //             flag = 1;
-        //             break;
-        //         }
-        //     }
-            
-        //     if (!flag) {
-        //         for (int k=0; k<i; k++){
-        //             if (nums[k] > nums[i]){
-        //                 nge.push_back(nums[k]);
-        //                 flag = 1;
-        //                 break;
-        //             }
-        //         }
-        //     }
-
-        //     if (!flag){
-        //         nge.push_back(-1);
-        //     }
-        // }
-        // return nge;
     }
 };

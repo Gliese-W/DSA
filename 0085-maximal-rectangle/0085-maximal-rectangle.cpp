f338b007fbf90a83d544f33ead14f5f6e4a66301
Gliese-W <always.tusharr@gmail.com>
2026-07-14 22:07:07 +0530

    Time: 55 ms (6.22%), Space: 18.4 MB (95%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -6,7 +6,6 @@ public:
         int M = matrix.size();
         int N = matrix[0].size();
 
-        // convert char to int (in-place)
         vector<vector<int>> mat(M, vector<int>(N));
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
@@ -14,7 +13,6 @@ public:
             }
         }
 
-        // row-wise prefix widths
         for (int i = 0; i < M; i++) {
             for (int j = 1; j < N; j++) {
                 if (mat[i][j] == 1) {
@@ -25,13 +23,11 @@ public:
 
         int Ans = 0;
 
-        // fix each column
         for (int j = 0; j < N; j++) {
             for (int i = 0; i < M; i++) {
                 int width = mat[i][j];
                 if (width == 0) continue;
 
-                // expand downward
                 int currWidth = width;
                 for (int k = i; k < M && mat[k][j] > 0; k++) {
                     currWidth = min(currWidth, mat[k][j]);
@@ -39,7 +35,6 @@ public:
                     Ans = max(Ans, currWidth * height);
                 }
 
-                // expand upward
                 currWidth = width;
                 for (int k = i; k >= 0 && mat[k][j] > 0; k--) {
                     currWidth = min(currWidth, mat[k][j]);

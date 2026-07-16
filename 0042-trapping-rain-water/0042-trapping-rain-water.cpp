7febb4e02b29a4580a617e2e410404600cf5963a
Gliese-W <always.tusharr@gmail.com>
2026-07-16 14:16:46 +0530

    Time: 0 ms (100%), Space: 26.1 MB (57.83%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
-
     int trap(vector<int>& height) {
 
-        int leftMax = 0, rightMax = 0, total = 0;
-        int l = 0, r = height.size()-1;
+        int start = 0;
+        int end = height.size()-1;
 
-        while(l<r){
-            if (height[l] <= height[r]){
+        int leftMax = 0;
+        int rightMax = 0;
 
-                if (leftMax > height[l]){
-                    total += leftMax - height[l];
-                } else{
-                    leftMax = height[l];
-                }
+        int totalWater = 0;
 
-                l++;
-            } else{
+        while (start < end){
 
-                if (height[r] < rightMax){
-                    total += rightMax - height[r];
-                } else{
-                    rightMax = height[r];
-                }
-                
-                r--;
+            leftMax = max(leftMax, height[start]);
+            rightMax = max(rightMax, height[end]);
+
+            if (leftMax < rightMax){
+                totalWater += leftMax - height[start];
+                start ++;
+            } else {
+                totalWater += rightMax - height[end];
+                end--;
             }
         }
 
-        return total;
+        return totalWater;
 
 
 
@@ -36,44 +32,27 @@ public:
 
 
 
-    // vector<int> prefixMax(vector<int>& height){
 
-    //     vector<int> preMax(height.size());
 
-    //     preMax[0] = height[0];
-    //     for (int i=1; i<height.size(); i++){
-    //         preMax[i] = max(preMax[i-1], height[i]);
-    //     }
 
-    //     return preMax;
-    // }
+        // int water = 0;
+        
+        // for (int i=0; i<height.size(); i++){
 
-    // vector<int> suffixMax(vector<int>& height){
-    //     vector<int> sufMax(height.size());
+            
+        //     int maxLeft = height[i];
+        //     for (int j=0; j<i; j++){
+        //         maxLeft = max(maxLeft, height[j]);
+        //     }
 
-    //     sufMax[height.size()-1] = height[height.size()-1];
-    //     for (int i=height.size()-2; i>=0; i--){
-    //         sufMax[i] = max(sufMax[i+1], height[i]);
-    //     }
+            
+        //     int maxRight = height[i];
+        //     for(int j=i+1; j<height.size(); j++){
+        //         maxRight = max(maxRight, height[j]);
+        //     }
 
-    //     return sufMax;
-    // }
-
-    // int trap(vector<int>& height) {
-
-    //     vector<int> preMax = prefixMax(height);
-    //     vector<int> sufMax = suffixMax(height);
-
-    //     int total = 0;
-    //     for (int i=0; i<height.size(); i++){
-
-    //         int leftMax = preMax[i];
-    //         int rightMax = sufMax[i];
-
-    //         if (height[i] < leftMax && height[i] < rightMax){
-    //             total += min(leftMax, rightMax) - height[i];
-    //         }
-    //     }
-    //     return total;
+        //     water += min(maxLeft, maxRight) - height[i];
+        // }
+        // return water;    
     }
 };

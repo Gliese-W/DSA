71b59270c39e11bd542fc6b8c77c5c2558413961
Gliese-W <always.tusharr@gmail.com>
2026-07-24 14:19:38 +0530

    Time: 27 ms (6.6%), Space: 14.2 MB (75.47%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,49 +1,18 @@
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
-
         long long total = 0;
-        for(int i=0; i<nums.size(); i++){
-            int s = INT_MAX;
-            int l = INT_MIN;
-            for(int j=i; j<nums.size(); j++){
-                if (nums[j]>l){
-                    l = nums[j];
-                }
-                if (nums[j]<s){
-                    s = nums[j];
-                }
-                total += l-s;
+
+        for (int i=0; i<nums.size(); i++){
+            int mini = nums[i];
+            int maxi = nums[i];
+
+            for (int j=i; j<nums.size(); j++){
+                maxi = max(maxi, nums[j]);
+                mini = min(mini, nums[j]);
+                total += (maxi-mini);
             }
         }
         return total;
-
-
-
-
-
-
-
-
-        // method 01 - O(n^3)  ;(
-
-
-        // long long total = 0;
-        // for(int i=0; i<nums.size(); i++){
-        //     for(int j=i; j<nums.size(); j++){
-        //         int s = INT_MAX;
-        //         int l = INT_MIN;
-        //         for(int k=i; k<=j; k++){
-        //             if (nums[k]>l){
-        //                 l = nums[k];
-        //             }
-        //             if (nums[k]<s){
-        //                 s = nums[k];
-        //             }
-        //         }
-        //         total += l-s;
-        //     }
-        // }
-        // return total;
     }
 };

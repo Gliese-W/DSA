e5e1a4f4cc94cd97ed133025aad9afa3701c6676
Gliese-W <always.tusharr@gmail.com>
2026-07-28 14:05:36 +0530

    Time: 116 ms (22.86%), Space: 75 MB (37.58%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+
+        if (s.size() <= 1) return s;
+
+        string first = "";
+        for (int i=0; i<s.size()/2; i++){
+            first += s[i];
+        }
+
+        sort(first.begin(), first.end());
+
+        // string second = "";
+
+        // for (int i=s.size()-1; i>s.size()/2; i--){
+        //     second += s[i];
+        // }
+
+        string second = first;
+
+        reverse(second.begin(), second.end());
+
+        // string mid = "";
+        // mid += s[(s.size()+1)/2];
+
+        if (s.size()%2==0){
+            return first + second;
+        }
+
+        return first + string(1, s[s.size()/2]) + second;
+    }
+};

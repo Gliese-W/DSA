3695b6c5b0f66742d5a7466b70579e2639fe30dc
Gliese-W <always.tusharr@gmail.com>
2026-08-09 15:23:42 +0530

    Time: 30 ms (21.12%), Space: 34.6 MB (17.1%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -2,23 +2,49 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
 
-        int dup = -1;
-        int missing = -1;
+        unordered_map<int, int> mpp;
 
         for (int i=1; i<=nums.size(); i++){
-            int count = 0;
-            for (int j=0; j<nums.size(); j++){
-                if (nums[j] == i){
-                    count ++;
-                }
-            }
-            if (count == 2){
-                dup = i;
-            }else if (count == 0){
-                missing = i;
+            mpp[i] = 0;
+        }
+
+        for (auto it : nums){
+            mpp[it] ++;
+        }
+
+        int dup = -1, missing = -1;
+        for (auto it : mpp){
+            if (it.second == 2){
+                dup = it.first;
+            } else if (it.second == 0){
+                missing = it.first;
             }
         }
 
         return {dup, missing};
+
+
+
+
+
+        // int dup = -1;
+        // int missing = -1;
+
+        // for (int i=1; i<=nums.size(); i++){
+        //     int count = 0;
+        //     for (int j=0; j<nums.size(); j++){
+        //         if (nums[j] == i){
+        //             count ++;
+        //         }
+        //     }
+
+        //     if (count == 2){
+        //         dup = i;
+        //     }else if (count == 0){
+        //         missing = i;
+        //     }
+        // }
+
+        // return {dup, missing};
     }
 };
